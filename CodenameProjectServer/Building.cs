d88efe0b439aedf91b37c64af781fac1210e8d02
Arne Haswell <sujomear@arcor.de>
2014-11-27 09:21:49 +0100

    to be overwritten 2

---
+++ CodenameProjectServer/Building.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodenameProjectServer
+{
+    class Building : SInterfaces.ISendable
+    {
+        //look at the nonimplementedness
+
+        public int Type
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Faction
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int ID
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public SFML.Window.Vector2f Position
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float Health
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public SFML.Window.Vector2f Center
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodenameProjectServer/SInterfaces.cs
@@ -0,0 +1,58 @@
+﻿using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodenameProjectServer
+{
+    class SInterfaces
+    {
+        /// <summary>
+        /// interface everything you want to send
+        /// has to implement
+        /// </summary>
+        public interface ISendable
+        {
+            //idea: WHAT, ID (NOT WHAT), WHERE, HEALTH (possibly reassigned correlating to WHAT parameter)
+            /// <summary>
+            /// Used for transmitting state
+            /// </summary>
+            int Type { get; }
+
+            /// <summary>
+            /// Used for transmitting state; contains the faction
+            /// </summary>
+            bool Faction { get; }
+
+            /// <summary>
+            /// This _MUST_ be set to the value of SGlobal.ID_COUNTER at time of creation,
+            /// which _HAS_ to be increased afterwards for my server protocol to work!
+            /// </summary>
+            int ID { get; set; }
+
+            /// <summary>
+            /// Used for transmitting state
+            /// </summary>
+            Vector2f Position { get; }
+            /// <summary>
+            /// Used for transmitting state
+            /// </summary>
+            float Health { get; }
+
+            /// <summary>
+            /// was pretty useful in the past,
+            /// just implement it
+            /// </summary>
+            Vector2f Center{ get; }
+
+            /// <summary>
+            /// generic function for updating.
+            /// the server will later send the four send-parameters to each client
+            /// </summary>
+            void Update();
+            
+        }
+    }
+}
