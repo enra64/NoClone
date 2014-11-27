2e8b5527cab08242bc397505c0b903e46e6ca6ce
Arne Haswell <sujomear@arcor.de>
2014-11-27 09:21:26 +0100

    to be overwritten

--- CodenameProjectServer/SGlobal.cs
+++ CodenameProjectServer/SGlobal.cs
@@ -8,6 +8,10 @@ namespace CodenameProjectServer
 {
     static class SGlobal
     {
+        /*
+         * SERVER GLOBAL FOR SERVER CONTAINING SERVER STUFF; DONT THINK THIS BELONGS TO CLIENT
+         */
         public const int MOUSE_CLICK_MESSAGE = 0, STRING_MESSAGE = 1, GAMESTATE_BROADCAST=2;
+        public static Int32 ID_COUNTER = 0;
     }
 }
---
+++ CodenameProjectServer/Server.cs
@@ -0,0 +1,160 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace CodenameProjectServer
+{
+    class Server
+    {
+        private const long UNDEFINED_CLIENT=-1;
+
+        private static NetServer netServer;
+        private static long clientIdentifier1 = UNDEFINED_CLIENT, clientIdentifier2 = UNDEFINED_CLIENT;
+        private static List<SInterfaces.ISendable> Sendlist = new List<SInterfaces.ISendable>();
+
+        static void Main(string[] args)
+        {
+            //debug, cant distinguish server/client xD
+            Console.WriteLine("server");
+
+            //need to create this b/c console app
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+
+            // set up network, name must be the same
+            NetPeerConfiguration config = new NetPeerConfiguration("CodenameProjectTwo");
+
+            config.MaximumConnections = 2;
+            config.Port = 14242;
+            netServer = new NetServer(config);
+            StartServer();
+            ServerLoop();
+        }
+
+        private static void ServerLoop()
+        {
+            while (true)
+            {
+                NetIncomingMessage im;
+                while ((im = netServer.ReadMessage()) != null)
+                {
+                    // handle incoming message
+                    switch (im.MessageType)
+                    {
+                        case NetIncomingMessageType.DebugMessage:
+                        case NetIncomingMessageType.ErrorMessage:
+                        case NetIncomingMessageType.WarningMessage:
+                        case NetIncomingMessageType.VerboseDebugMessage:
+                            string text = im.ReadString();
+                            //Console.WriteLine("d/e/w/v: "+text);
+                            break;
+
+                        case NetIncomingMessageType.StatusChanged:
+                            NetConnectionStatus status = (NetConnectionStatus)im.ReadByte();
+
+                            string reason = im.ReadString();
+                            Console.WriteLine(NetUtility.ToHexString(im.SenderConnection.RemoteUniqueIdentifier) + " " + status + ": " + reason);
+
+                            if (status == NetConnectionStatus.Connected)
+                            {
+                                //save uid
+                                string hail="";
+                                if (clientIdentifier1 == UNDEFINED_CLIENT){
+                                    clientIdentifier1 = im.SenderConnection.RemoteUniqueIdentifier;
+                                    hail = "Client 1 (" + clientIdentifier1 + ")";
+                                }
+                                else if (clientIdentifier2 == UNDEFINED_CLIENT)
+                                {
+                                    clientIdentifier2 = im.SenderConnection.RemoteUniqueIdentifier;
+                                    hail = "Client 2 (" + clientIdentifier2 + ")";
+                                }
+                                Console.WriteLine(hail);
+                            }
+                            break;
+                        case NetIncomingMessageType.Data:
+                            // incoming chat message from a client
+                            //debug identifying clients
+                            if (im.SenderConnection.RemoteUniqueIdentifier == clientIdentifier1)
+                                Console.Write("client 1: ");
+                            else if (im.SenderConnection.RemoteUniqueIdentifier == clientIdentifier2)
+                                Console.Write("client 2: ");
+                            else
+                                Console.WriteLine("unknown client detected");
+                            //identify message
+                            switch (im.ReadInt32())
+                            {
+                                case SGlobal.MOUSE_CLICK_MESSAGE:
+                                    Console.WriteLine("Mouseclick at: "+im.ReadFloat()+", "+im.ReadFloat());
+                                    break;
+                                case SGlobal.STRING_MESSAGE:
+                                    Console.WriteLine("got string message: " + im.ReadString());
+                                    break;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Unhandled type: " + im.MessageType + " " + im.LengthBytes + " bytes " + im.DeliveryMethod + "|" + im.SequenceChannel);
+                            break;
+                    }
+                    netServer.Recycle(im);
+                }
+
+                /*
+                 BROADCAST GAME STATE
+                 */
+                List<NetConnection> all = netServer.Connections; // get copy
+                if (all.Count > 0){
+                    NetOutgoingMessage om = netServer.CreateMessage();
+                    /*
+                     Write everything the clients need to know on each update.
+                     * Currently we broadcast everything all the time; this
+                     * gets more and more inefficient the more stuff we need to
+                     * send, prompting a data-identifier if the game begins to lag
+                     */
+                    //identify as server broadcast
+                    om.Write(SGlobal.GAMESTATE_BROADCAST);
+                    //protocol: send the type first, then an unique identifier (see SGlobal.ID_COUNT)
+                    foreach (SInterfaces.ISendable s in Sendlist)
+                    {
+                        Console.WriteLine("server send");
+                        om.Write(s.Type);
+                        om.Write(s.Faction);
+                        om.Write(s.ID);
+                        om.Write(s.Position.X);
+                        om.Write(s.Position.Y);
+                        om.Write(s.Health);
+                    }
+
+                    om.Write(27);
+                    om.Write(false);
+                    om.Write(0);//id, should begin with 0
+                    om.Write(5f);
+                    om.Write(10f);
+                    om.Write(10f);
+                    //declare message end
+                    Console.WriteLine("server send");
+                    om.Write(-1);
+                    if (Sendlist.Count == 0)
+                        netServer.SendMessage(om, all, NetDeliveryMethod.Unreliable, 0);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        // called by the UI
+        public static void StartServer()
+        {
+            netServer.Start();
+        }
+
+        // called by the UI
+        public static void Shutdown()
+        {
+            netServer.Shutdown("Requested by user");
+        }
+    }
+}
