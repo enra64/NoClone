a1e2da3e4d35f358676f138a0808a0f6d4b4f1f9
gSilas <d.micheel@web.de>
2014-12-02 12:07:08 +0100

    - missed th interface stuff :><

--- CodenameProjectTwo/buildings/VillageCentre.cs
+++ CodenameProjectTwo/buildings/VillageCentre.cs
@@ -8,18 +8,41 @@ namespace CodenameProjectTwo.buildings
 {
     class VillageCentre
     {
-        Texture hqtexr = new Texture("assets/graphics/buildings/hqred.png");
-        Texture hqtexb = new Texture("assets/graphics/buildings/hqblue.png");
+        private RenderWindow win;
+        
+        private Sprite hqr;
+        private Sprite hqb;
 
-
-        void Update
+        public VillageCentre(RenderWindow _w, String color)
         {
+            win = _w;
+            if (color == "red")
+            {
+                Texture hqtexr = new Texture("assets/graphics/buildings/hqred.png");
+                hqr = new Sprite(hqtexr);
+            }
 
+            else
+            {
+                Texture hqtexb = new Texture("assets/graphics/buildings/hqblue.png");
+                hqb = new Sprite(hqtexb);
+            }
         }
 
-        void Draw
+        public void Update()
         {
+            
+            Vector2f hqrPosition = new Vector2f(0f, 0f);
+            Vector2f hqbPosition = new Vector2f(100f, 100f);
+            hqr.Position = hqrPosition;
+            hqb.Position = hqbPosition;
+        }
 
+        public void Draw()
+        {
+            win.Draw(hqr);
+            win.Draw(hqb);
+            win.Display();
         }
     }
 }
