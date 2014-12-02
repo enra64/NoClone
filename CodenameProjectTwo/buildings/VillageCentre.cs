2259aa3152ac279f7e6857d99d3e3e1e8c92e7f5
gSilas <d.micheel@web.de>
2014-12-02 11:42:51 +0100

    - hq

--- CodenameProjectTwo/buildings/VillageCentre.cs
+++ CodenameProjectTwo/buildings/VillageCentre.cs
@@ -8,5 +8,18 @@ namespace CodenameProjectTwo.buildings
 {
     class VillageCentre
     {
+        Texture hqtexr = new Texture("assets/graphics/buildings/hqred.png");
+        Texture hqtexb = new Texture("assets/graphics/buildings/hqblue.png");
+
+
+        void Update
+        {
+
+        }
+
+        void Draw
+        {
+
+        }
     }
 }
