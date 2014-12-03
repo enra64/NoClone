f1905e3ec754abe5a1b5ec0520380930197d07cd
gSilas <d.micheel@web.de>
2014-12-03 23:08:57 +0100

    hahahahaha madness ensued still no idea

---
+++ CodenameProjectServer/Centre.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace CodenameProjectServer
+{
+    class Centre : CodenameProjectServer.SInterfaces.ISendable
+    {
+        public int Type { get; private set; }
+        public int ID { get; set; }
+        public bool Faction { get; set; }
+        public Vector2f Position { get; set; }
+        public float Health { get; set; }
+
+        public Centre(int _type, bool _faction, int _ID, Vector2f _position, float _health)
+        {
+            Type = _type;
+            ID = _ID;
+            Faction = _faction;
+            Position = _position;
+            Health = _health;
+        }
+
+        public SFML.Window.Vector2f Center
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public SFML.Window.Vector2f Target
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool UpdateNeeded
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
