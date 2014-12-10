630a616c7ca4c9ba0996060b26cce5de9e236fbe
Arne Herdick <sujomear@arcor.de>
2014-12-10 16:50:07 +0100

    pushing class visual studio took out of  commit -.-

---
+++ CodenameProjectServer/Buildings/AbstractServerBuilding.cs
@@ -0,0 +1,86 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodenameProjectServer.Buildings {
+    abstract class AbstractServerBuilding : SInterfaces.ISendable {
+        #region variables
+        //use like variables
+        /// <summary>
+        /// type of the building; you can use either one class for multiple types or
+        /// one type per class; the classes will be instanced via a switch() listening
+        /// to the type
+        /// </summary>
+
+        public abstract int Type { get; set; }
+        /// <summary>
+        /// the id of this instance; dont change it, because the server
+        /// ensures that it is unique. 
+        /// if you do change it, the game will crash
+        /// because of you. how does that make you feel?
+        /// </summary>
+
+        public abstract int ID { get; set; }
+        /// <summary>
+        /// False->client 1; true-> client 2. probably will bite us in the ass
+        /// as soon as we want more than two players.
+        /// </summary>
+        public abstract byte Faction { get; set; }
+
+        /// <summary>
+        /// gets set on instanciation, is the position of your building
+        /// </summary>
+        public abstract Vector2f Position { get; set; }
+
+        /// <summary>
+        /// gets set to 100 on instanciation; please stay between 0 and 100.
+        /// </summary>
+        public abstract float Health { get; set; }
+        public abstract bool UpdateNeeded { get; set; }
+        public abstract int TargetID { get; set; }
+        public abstract Vector2f Target { get; set; }
+        public abstract bool implementAggroOrEffectEffects { get; set; }
+
+        /*
+         * VARIABLES END HERE
+         */
+        #endregion
+
+        protected AbstractServerBuilding(int _type, byte _faction, int _ID, Vector2f _position, float _health) {
+            Type = _type;
+            ID = _ID;
+            Faction = _faction;
+            Position = _position;
+            Health = _health;
+        }
+
+        /// <summary>
+        /// a short name that is displayed by the id
+        /// btw dont use umlauts
+        /// </summary>
+        public string Name { get; set; }
+
+        public FloatRect aggroRectangle {
+            get { return new FloatRect(this.Position.X - SGlobal.AGGRO_RECT_SIZE / 2, this.Position.Y - SGlobal.AGGRO_RECT_SIZE / 2, SGlobal.AGGRO_RECT_SIZE, SGlobal.AGGRO_RECT_SIZE); }
+        }
+
+        public FloatRect effectiveRectangle {
+            get {
+                return new FloatRect(this.Position.X - SGlobal.EFFECTIVE_RECT_SIZE / 2, this.Position.Y - SGlobal.EFFECTIVE_RECT_SIZE / 2, SGlobal.EFFECTIVE_RECT_SIZE, SGlobal.EFFECTIVE_RECT_SIZE);
+            }
+        }
+
+        /// <summary>
+        /// Draw your building...
+        /// </summary>
+        public abstract void Update();
+
+        public abstract void TakeEffect(int itemID);
+
+        public abstract void TargetAggro(int itemID);
+    }
+}
